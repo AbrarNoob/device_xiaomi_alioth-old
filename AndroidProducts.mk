ee2967b30976c7960b4ec9e1dca94abe6bd0188c
AbrarNoob <hasinabrarhaque@gmail.com>
2022-08-08 12:49:32 +0600

    banana-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_alioth.mk
+    $(LOCAL_DIR)/banana_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_alioth-user \
-    blaze_alioth-userdebug \
-    blaze_alioth-eng
+    banana_alioth-user \
+    banana_alioth-userdebug \
+    banana_alioth-eng
--- banana_alioth.mk
+++ banana_alioth.mk
@@ -8,19 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Blaze stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common Banana stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-#Blaze Stuffs
-TARGET_BOOT_ANIMATION_RES := 1440
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := 𝗔𝗕𝗥𝗔𝗥
-TARGET_USE_PIXEL_CHARGER := true
+#Banana Stuffs
+BANANA_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := blaze_alioth
+PRODUCT_NAME := banana_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
